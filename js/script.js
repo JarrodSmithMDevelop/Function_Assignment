bf713acdb2c5a33c3f9da3c1a568fd7d8955dde1
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-21 23:35:42 -0500

    building functions
    I am working on building the functions for Florida lotto

--- js/script.js
+++ js/script.js
@@ -8,8 +8,34 @@
 
 //Variables
 
-//Inputs
+var lottery = true;
+var lottoNum;
 
 //Functions
 
+//Function for Florida Lottery
+function lotteryNumGen(max , min){
+    var flLotto = [];
+    for (var i = 0; i < 7; i++ ) {
+        flLotto[i] = Math.random() * (max - min) + min;
+        //Round up lotto numbers
+        flLotto = Math.round(flLotto);
+        console.log (flLotto)
+    }
+    return flLotto
+}
+//Inputs
+
+lottery = confirm("For the Powerball number select \"OK\". For the Florida Lottery numbers select \"Cancel\"");
+
+//Main Code
+
+if (lottery === true){
+
+
+}else{
+    lottoNum = lotteryNumGen(53,1);
+}
 //Outputs
+
+console.log(lottoNum);
