dd88912a07113779801ee023aa6dfeec4740873d
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-22 00:23:55 -0500

    building
    I am still building on my code. Working out my math with in my
    functions so I do not ever get the same numbers 2 times.

--- js/script.js
+++ js/script.js
@@ -14,15 +14,34 @@ var lottoNum;
 //Functions
 
 //Function for Florida Lottery
-function lotteryNumGen(max , min){
+function flLotteryNumGen(max , min){
     var flLotto = [];
     for (var i = 0; i < 7; i++ ) {
-        flLotto[i] = Math.random() * (max - min) + min;
+
+        Number(flLotto[i] = Math.random(flLotto) * (max - min) + min);
         //Round up lotto numbers
-        flLotto = Math.round(flLotto);
-        console.log (flLotto)
+        flLotto[i] = Math.round(flLotto[i]);
+
     }
     return flLotto
+}
+
+function powLotteryNumGen(max , min){
+    var powLotto = [];
+    for (var i = 0; i < 5; i++ ) {
+
+        powLotto[i] = Math.random() * (max - min) + min;
+        //Round up lotto numbers
+        powLotto[i] = Math.round(powLotto[i]);
+            var powerball
+            for (var ind = 0; ind = 1 ;ind++){
+                powerball = Math.random() * (35 - 1) +1;
+                //Round up lotto numbers
+                powerball = Math.round();
+            }
+
+    }
+    return powLotto
 }
 //Inputs
 
@@ -31,11 +50,12 @@ lottery = confirm("For the Powerball number select \"OK\". For the Florida Lotte
 //Main Code
 
 if (lottery === true){
-
+    lottoNum = powLotteryNumGen(59,1);
 
 }else{
-    lottoNum = lotteryNumGen(53,1);
+    lottoNum = flLotteryNumGen(10,1);
 }
+
 //Outputs
 
 console.log(lottoNum);
